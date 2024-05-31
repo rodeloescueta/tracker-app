323155ff230409f9b886b4c3e5684027e9432b2e
Rodelo Escueta <rodelo.escueta@outgive.ca>
2024-05-31 17:36:15 +0800

    remove background

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -49,7 +49,7 @@ export default async function Articles({ params }: { params: { id: string } }) {
             />
           </CardContent>
         </Card>
-        <BackgroundBeams />
+        {/* <BackgroundBeams /> */}
       </div>
     </div>
   );
